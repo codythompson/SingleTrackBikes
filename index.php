d4140ee846636b6e559429b588c398ae5564e6cc
Cody <cody.thompson22@gmail.com>
2013-06-07 00:46:20 -0700

    Added the carousel to the PHP version of the home page

--- index.php
+++ index.php
@@ -4,10 +4,11 @@ require_once("cms/navbar.php");
 require_once("cms/page.php");
 
 $title = "Single Track Bikes - Flagstaff Arizona";
-$cssHrefs = array();
+$cssHrefs = array(
+    "/styles/st-carousel.css");
 $jsSrcs = array();
 $navBar = new NavBar(GetNavLinks(), 0);
-$content = "";
+$content = "content/home.php";
 
 MakePage($title, $cssHrefs, $jsSrcs, $navBar, $content);
 ?>
