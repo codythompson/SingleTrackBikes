ba504510d9451225d593e087dc6d3875505fa63a
Cody <cody.thompson22@gmail.com>
2013-06-09 16:53:21 -0700

    Added a page indicator and slide arrows to the 'carousel'

--- content/home.php
+++ content/home.php
@@ -1,5 +1,12 @@
                     <div id="big-carousel" class="carousel slide">
+                        <ol class="carousel-indicators st-rounded">
+                            <li data-target="#big-carousel" data-slide-to="0" class="active"></li>
+                            <li data-target="#big-carousel" data-slide-to="1"></li>
+                            <li data-target="#big-carousel" data-slide-to="2"></li>
+                        </ol>
+
                         <div class="carousel-inner st-rounded">
+
                             <div class="active item">
                                 <img src="images/Front_COUNTER.JPG" alt="Single Track Bikes" />
                                 <div class="st-carousel-caption st-carousel-caption-center st-rounded">
@@ -9,6 +16,7 @@
                                     This Item is centered - woo hoo
                                 </div>
                             </div>
+
                             <div class="item">
                                 <img src="images/orange_1.jpg" alt="Single Track Bikes" />
                                 <div class="st-carousel-caption st-carousel-caption-topleft st-rounded">
@@ -18,6 +26,7 @@
                                     This Item is top-left centered - woo hoo
                                 </div>
                             </div>
+
                             <div class="item">
                                 <img src="images/GOPR0030.JPG" alt="Single Track Bikes" />
                                 <div class="st-carousel-caption st-carousel-caption-bottomright st-rounded">
@@ -36,6 +45,16 @@
                                     This Item is bottom-right centered - woo hoo
                                 </div>
                             </div>
+
                         </div>
+
+                        <a class="carousel-control-left left" href="#big-carousel"
+                                data-slide="prev">
+                            <img src="/images/st-arrow-left.png" alt="scroll left"/>
+                        </a>
+                        <a class="carousel-control-right right" href="#big-carousel"
+                                data-slide="next">
+                            <img src="/images/st-arrow-right.png" alt="scroll right"/>
+                        </a>
                     </div>
                     <!-- end carousel -->
